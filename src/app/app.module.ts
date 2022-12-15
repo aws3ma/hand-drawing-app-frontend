920f03d15ebee2db49114b589c724a04ff6ebda8
Oussema Trabelsi <79041057+aws3ma@users.noreply.github.com>
2022-12-15 11:56:19 +0100

    add tailwind and intialise required componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { InscriptionComponent } from './auth/inscription/inscription.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { HistoryComponent } from './history/history.component';
+import { CompareComponent } from './compare/compare.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InscriptionComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    HistoryComponent,
+    CompareComponent
   ],
   imports: [
     BrowserModule,
